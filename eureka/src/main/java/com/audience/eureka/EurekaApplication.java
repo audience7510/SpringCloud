8d33ba3c17e1cf46771b63172efe71e53500fbd0
Administrator <audience7510@163.com>
2018-12-05 18:57:24 +0800

    创建zuul

--- eureka/src/main/java/com/audience/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/audience/eureka/EurekaApplication.java
@@ -2,11 +2,14 @@ package com.audience.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
+        System.out.println("*****Eureka服务启动成功*****");
     }
 }
